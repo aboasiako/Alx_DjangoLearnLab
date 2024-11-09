acccfad03811e905282c4bc2b748afd1734144a9
Alfred <akojo2273@gmail.com>
2024-11-09 14:26:38 +0000

    "File Update"

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,8 @@ from django.views.generic import DetailView
 from .models import Book, Library
 from .models import Library
 from django.views.generic.detail import DetailView
+from .views import list_books
+
 
 
 # Function-based view to list all books
