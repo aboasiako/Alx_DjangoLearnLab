8a4337fae8149fcc25ececb7110876a78424aafb
Alfred <akojo2273@gmail.com>
2024-11-14 03:22:38 +0000

    "File Update "

--- advanced_features_and_security/settings.py
+++ advanced_features_and_security/settings.py
@@ -1,12 +1,3 @@
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
-AUTH_USER_MODEL = 'your_app_name.CustomUser'
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'bookshelf'
-]
+
