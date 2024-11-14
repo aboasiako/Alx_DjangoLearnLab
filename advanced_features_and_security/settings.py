288d57caee44b5fde41689f2c8fb8eb3ede36e3f
Alfred <akojo2273@gmail.com>
2024-11-14 03:13:14 +0000

    Move bookshelf app into LibraryProject

--- advanced_features_and_security/settings.py
+++ advanced_features_and_security/settings.py
@@ -1,1 +1,10 @@
 AUTH_USER_MODEL = 'your_app_name.CustomUser'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'bookshelf'
+]
